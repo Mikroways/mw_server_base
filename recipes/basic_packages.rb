df1721c7501e934b7d4354b2be58df6f847977f1
Christian Rodriguez <chrodriguez@gmail.com>
2017-02-20 15:57:56 -0300

    remove atsar debian package

--- recipes/basic_packages.rb
+++ recipes/basic_packages.rb
@@ -1,7 +1,7 @@
 # Installs some useful tools on a server.
 basic_packages =
   value_for_platform_family(
-    debian: %w(atsar vim locate apt-utils bsdutils htop rsync iftop iotop telnet tcpdump strace sysstat ngrep tmux iproute haveged),
+    debian: %w(vim locate apt-utils bsdutils htop rsync iftop iotop telnet tcpdump strace sysstat ngrep tmux iproute haveged),
     rhel: %w(vim mlocate htop rsync iftop iotop telnet tcpdump strace sysstat ngrep tmux iproute haveged)
   )
 
